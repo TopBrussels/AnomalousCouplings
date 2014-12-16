cbd4d64a5f61cc95aa62247cbc14ec7d918da91e
Annik <annik.olbrechts@cern.ch>
2014-12-16 14:23:31 +0100

    Started from a fresh directory to get rid off the Z2Volume references

---
+++ ttbarSemiMuPlus_QED2/Source/MODEL/couplings2.f
@@ -0,0 +1,17 @@
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c      written by the UFO converter
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      SUBROUTINE COUP2()
+
+      IMPLICIT NONE
+
+      DOUBLE PRECISION PI
+      PARAMETER  (PI=3.141592653589793D0)
+      INCLUDE 'input.inc'
+      INCLUDE 'coupl.inc'
+
+      INCLUDE 'model_functions.inc'
+      GC_6 = -G
+      GC_7 = COMPLEXI*G
+      END
---
+++ ttbarSemiMuPlus_QED2/SubProcesses/P1_gg_ttx_t_bwp_wp_mupvm_tx_bxwm_wm_qq/multi_channel.f
@@ -0,0 +1,50 @@
+C+-----------------------------------------------------------------------+
+C|                MULTI-CHANNEL WEIGHT FOR MADWEIGHT                     |
+C|                                                                       |
+C|     Author: Pierre Artoisenet (UCL-CP3)                               |
+C|             Olivier Mattelaer (UCL-CP3)                               |
+C+-----------------------------------------------------------------------+
+C|     This file is generated automaticly by MADWEIGHT-ANALYZER          |
+C+-----------------------------------------------------------------------+
+
+       double precision function multi_channel_weight(config)
+C+-----------------------------------------------------------------------+
+C|     routine returnings the multi channel weight linked to the         |
+C|       change of variable 'config'                                     |
+C+-----------------------------------------------------------------------+
+       	implicit none
+
+       	integer config
+       	include 'coupl.inc'
+           include 'd_choices.inc'
+c    double precision prov1,prov2,prov3,prov4,prov5,prov6,prov7,prov8,prov9
+c    double precision prov10,prov11,prov12,prov13,prov14,prov15,prov16
+c    double precision prov17,prov18,prov19,prov20,prov21,prov22,prov23 
+           double precision num,den
+           double precision zero,one
+           parameter (zero=0d0)
+           parameter (one=1d0)
+        double precision local_1
+        double precision  Breit_Wigner_for_part
+        external  Breit_Wigner_for_part
+        double precision local_2
+
+        if (config.eq.1) then
+        local_1 = Breit_Wigner_for_part( -3, MW, WW)
+        local_2 = Breit_Wigner_for_part( -4, MT, WT)
+
+        num = 1d0 * 1d0 * local_1
+        den = 0d0 + 1d0 * local_1 + 1d0 * local_2
+        multi_channel_weight = num/den
+
+        elseif (config.eq.2) then
+        local_1 = Breit_Wigner_for_part( -3, MW, WW)
+        local_2 = Breit_Wigner_for_part( -4, MT, WT)
+
+        num = 1d0 * 1d0 * local_2
+        den = 0d0 + 1d0 * local_1 + 1d0 * local_2
+        multi_channel_weight = num/den
+       	   endif
+       	   return
+       	   end
+
