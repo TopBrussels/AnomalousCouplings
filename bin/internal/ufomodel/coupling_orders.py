7e901769087273304becde5a79c4640935912b1d
Annik <annik.olbrechts@cern.ch>
2015-07-02 12:03:14 +0200

    Adding MadWeight files to github branch

---
+++ bin/internal/ufomodel/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 1.6.11
+# Mathematica version: 9.0 for Linux x86 (64-bit) (February 7, 2013)
+# Date: Thu 22 Aug 2013 11:27:13
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 2,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+TEST = CouplingOrder(name = 'TEST',
+                     expansion_order = 99,
+                     hierarchy = 2)
+
